43156adf5819a5891795af061a20f384d89d6db0
os <c1216440698@126.com>
2022-05-04 01:09:31 -0700

    hw04

---
+++ hw04/parsons_probs/smartfridge.py
@@ -0,0 +1,29 @@
+class SmartFridge:
+    """"
+    >>> fridgey = SmartFridge()
+    >>> fridgey.add_item('Mayo', 1)
+    'I now have 1 Mayo'
+    >>> fridgey.add_item('Mayo', 2)
+    'I now have 3 Mayo'
+    >>> fridgey.use_item('Mayo', 2.5)
+    'I have 0.5 Mayo left'
+    >>> fridgey.use_item('Mayo', 0.5)
+    'Uh oh, buy more Mayo!'
+    """
+
+    def __init__(self):
+        self.items = {}
+
+    def add_item(self, item, quantity):
+        if item in self.items:
+            self.items[item] += quantity
+        else:
+            self.items[item] = quantity
+        print('I now have ' + str(self.items[item]) + " " + item)
+
+    def use_item(self, item, quantity):
+        if item not in self.items or self.items[item] < quantity:
+            print('Uh oh, buy more Mayo!')
+        else:
+            self.items[item] -= quantity
+            print('I now have ' + str(self.items[item]) + " " + item + " left")
