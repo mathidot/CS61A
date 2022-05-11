08b0e7e110684b4cb906704cc3b4c0ca51a14550
mathidot <c1216440698@126.com>
2022-05-11 00:30:09 -0700

    lab10-final

--- lab10/lab10.scm
+++ lab10/lab10.scm
@@ -1,10 +1,23 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+        (cond
+            ((< num1 num2) -1)
+            ((= num1 num2) 0)
+            ((> num1 num2) 1)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num)
+    (define (adder inc)
+        (+ num inc))
+    adder)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g)
+    (lambda (x) (f (g x))))
 
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
-
+(define (pow base exp)
+    (cond
+        ((equal? exp 0) 1)
+        ((equal? (remainder exp 2) 1) (* base (pow base (- exp 1))))
+        ((equal? (remainder exp 2) 0) (square (pow base (/ exp 2))))
+    ))
+    
